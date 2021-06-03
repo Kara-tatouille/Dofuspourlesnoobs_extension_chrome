c337becb1a4df493ec27e25ee7f105b334288685
corenbla <corenbla@gmail.com>
2021-06-03 11:44:35 +0200

    Added support for dofus-map

--- content.js
+++ content.js
@@ -1,19 +1,78 @@
-const paragraphs = [...document.querySelectorAll('.paragraph')]
-    .filter(div => {
-        return div.textContent.match(/\[.*]/gm) !== null;
-    })
-;
+const {host} = window.location;
 
-paragraphs.forEach(paragraph => {
-    const regex = /(\[[^\[]*])/g;
-    const str = paragraph.innerHTML;
-    const subst = `<span class="position" style="font-size: 1.1em; color: hotpink; font-weight: bold; background-color: aliceblue; border-radius: 10px; padding: 5px; cursor: pointer">$1</span>`;
-    paragraph.innerHTML = str.replace(regex, subst)
-});
+switch (host) {
+    case 'www.dofuspourlesnoobs.com':
+        dofuspourlesnoobs();
+        break;
+    case 'dofus-map.com':
+        dofusmap();
+        break;
+}
 
-document.querySelectorAll('.position').forEach(position => {
-    position.addEventListener('click', event => {
-        const str = '/travel ' + event.target.textContent.replace(/[\[\]]/g, '')
+document.addEventListener('click', (event) => {
+    const position = event.target.closest('.position');
+    if (position !== null) {
+        const str = position.dataset.travel;
         navigator.clipboard.writeText(str);
-    })
+    }
 })
+
+function dofuspourlesnoobs() {
+    const paragraphs = [...document.querySelectorAll('.paragraph')]
+        .filter(div => {
+            return div.textContent.match(/\[.*]/gm) !== null;
+        })
+    ;
+
+    paragraphs.forEach(paragraph => {
+        const regex = /(\[([^\[]*)])/g;
+        const str = paragraph.innerHTML;
+        const subst = `
+            <span 
+                class="position"
+                data-travel="/travel $2"
+                title="Copier la commande d'autopilotage"
+                style="font-size: 1.1em; color: hotpink; font-weight: bold; background-color: aliceblue; border-radius: 10px; padding: 5px; cursor: pointer"
+            >$1</span>`;
+        paragraph.innerHTML = str.replace(regex, subst);
+    });
+}
+
+function dofusmap() {
+    const targetNode = document.getElementById('secondLine');
+    const observer = new MutationObserver((records) => {
+        records.forEach((record) => {
+            let texts = [];
+
+            record.addedNodes.forEach((node) => {
+                texts.push(node.textContent.trim())
+            })
+
+            texts = texts.map((text) => {
+                return Number.parseInt(text.trim())
+            });
+            const coordinates = texts.filter((item) => {
+                return !Number.isNaN(item);
+            });
+
+            if (coordinates.length === 2) {
+                // TODO : replace all this with a stylesheet
+                targetNode.dataset.travel = `/travel ${coordinates[0]},${coordinates[1]}`
+                targetNode.classList.add('position')
+                targetNode.style.color = 'hotpink';
+                targetNode.style.backgroundColor = '#171717';
+                targetNode.style.borderRadius = '10px';
+                targetNode.style.padding = '5px';
+                targetNode.style.margin = '5px';
+                targetNode.style.cursor = 'pointer';
+                targetNode.style.display = 'inline-block';
+                targetNode.title = 'Copier la commande d\'autopilotage'
+            }
+        })
+    });
+
+    observer.observe(targetNode, {
+        subtree: true,
+        childList: true,
+    });
+}
